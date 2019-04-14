a26c9a4d97ca064c6a82d4345ea7e765ee6c21e6
KittiShane <kittichotekamalapirat@Kittichotes-MacBook-Air.local>
2019-04-14 19:15:50 +0900

    finish responsive navbar

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,31 +2,33 @@
 
 // make background change
 
-const symbols = ['HTML', 'CSS', 'JavaScript','Ai'];
-let count = 0;
-const element = document.getElementById("change");
-const iteration = () => {
-  element.innerHTML = symbols[parseInt(count / 2, 10) % symbols.length];
-  if (count % 2 !== 0) {
-    element.classList.add("out");
-  } else {
-    element.classList.remove("out");
-  }
-  count++;
-  // if you're affraid the code could run for so long that it could fail,
-  // 6 is enough (number of elems * 2)
-  if (count === symbols.length * 2) {
-      count = 0;
-  }
-};
-let inthandle = setInterval(iteration, 1000);
-iteration();
+// const symbols = ['HTML', 'CSS', 'JavaScript','Ai'];
+// let count = 0;
+// const element = document.getElementById("change");
+// const iteration = () => {
+//   element.innerHTML = symbols[parseInt(count / 2, 10) % symbols.length];
+//   if (count % 2 !== 0) {
+//     element.classList.add("out");
+//   } else {
+//     element.classList.remove("out");
+//   }
+//   count++;
+//   // if you're affraid the code could run for so long that it could fail,
+//   // 6 is enough (number of elems * 2)
+//   if (count === symbols.length * 2) {
+//       count = 0;
+//   }
+// };
+// let inthandle = setInterval(iteration, 1000);
+// iteration();
 
+// responsive navigation
+// turn 3 stripes to X
+$("#hamburger").click(function(){
+  $("#hamburger").toggleClass("is-active")
+});
 
-// case page
-const medEasyThumbnail = getElementById('#medeasy')
-
-$( "#medeasy ).hover(function() {
-  $( this ).fadeOut( 100 );
-  $( this ).fadeIn( 500 );
+// toggle nav bar
+$("#hamburger").click(function(){
+  $("ul").toggleClass("active")
 });
