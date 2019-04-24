f8f9bd53d68b7d05ee733f2a71dd9c397c81451b
KittiShane <kittichotekamalapirat@Kittichotes-MacBook-Air.local>
2019-04-24 23:58:14 +0900

    responsive case page

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-app.set("view engine", "ejs");
-
+app.set('view engine', 'ejs');
 
 var morgan = require("morgan");
 app.use(morgan("tiny")); // less text in our logs??
@@ -10,8 +9,9 @@ app.use(morgan("tiny")); // less text in our logs??
 // no need to use app.use(app.router) because of that update
 // function signature express.static(root, [options])
 app.use(express.static(__dirname + '/public'));
+
 // mount root to views/statics
-app.use('/', express.static('views/statics'));
+// app.use('/', express.static('views/statics'));
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,40 +20,49 @@ app.listen(PORT,() => {
   });
 
 
-app.get('/case',(req,res,next)=> {
-  res.sendFile('case.html', { root: path.join( __dirname, 'views/statics')})
+
+
+  // index page 
+app.get('/', function(req, res) {
+  res.render('statics/index');
+});
+
+app.get('/case', function(req, res) {
+  res.render('statics/case');
+});
+app.get('/project',(req,res,next)=> {
+  res.render('statics/project')
 
 })
 
+
+
+
 app.get('/case/junction', (req, res) => {
-  res.sendFile('junction.html',{ root: path.join( __dirname, 'views/statics')});
+  res.render('statics/junction')
 });
 
 app.get('/case/tedx-website', (req, res) => {
-  res.sendFile('tedx-website.html',{ root: path.join( __dirname, 'views/statics')});
+  res.render('statics/tedx-website')
 });
 
 app.get('/case/portfolio-website', (req, res) => {
-  res.sendFile('portfolio-website.html',{ root: path.join( __dirname, 'views/statics')});
+  res.render('statics/portfolio-website');
 });
 
 app.get('/case/gochiso', (req, res) => {
-  res.sendFile('gochiso.html',{ root: path.join( __dirname, 'views/statics')});
+  res.render('statics/gochiso')
 });
 
 app.get('/case/tedx-flyer', (req, res) => {
-  res.sendFile('tedx-flyer.html',{ root: path.join( __dirname, 'views/statics')});
+  res.render('statics/tedx-flyer')
 });
 
 app.get('/case/thirty-logo-challenge', (req, res) => {
-  res.sendFile('thirty-logo-challenge.html',{ root: path.join( __dirname, 'views/statics')});
+  res.render('statics/thirty-logo-challenge')
 });
 
 
-app.get('/project',(req,res,next)=> {
-  res.sendFile('project.html', { root: path.join( __dirname, 'views/statics')})
-
-})
 
 
 app.get('/project/portfolio-website',(req,res,next)=> {
