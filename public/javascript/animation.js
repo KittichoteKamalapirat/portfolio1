fdb26ae6fd09a19b4bd7c10a8462f6c355a5984f
KittiShane <kittichotekamalapirat@Kittichotes-MacBook-Air.local>
2019-05-02 19:14:32 +0900

    add animation background

---
+++ public/javascript/animation.js
@@ -0,0 +1,62 @@
+// make background change
+
+const symbols = ['Ai', 'CSS', 'JavaScript','HTML',"jQuery","Node"];
+let count1 = 0;
+const element1 = document.getElementById("change1");
+const iteration1 = () => {
+  element1.innerHTML = symbols[parseInt(count1 / 2, 10) % symbols.length];
+  if (count1 % 2 !== 0) {
+    element1.classList.add("out");
+  } else {
+    element1.classList.remove("out");
+  }
+  count1++;
+  // if you're affraid the code could run for so long that it could fail,
+  // 6 is enough (number of elems * 2)
+  if (count1 === symbols.length * 2) {
+      count1 = 0;
+  }
+};
+let inthandle1 = setInterval(iteration1, 2500);
+iteration1();
+
+// second one 
+let count2 = 5;
+const element2 = document.getElementById("change2");
+const iteration2 = () => {
+  element2.innerHTML = symbols[parseInt(count2 / 2, 10) % symbols.length];
+  if (count2 % 2 !== 0) {
+    element2.classList.add("out");
+  } else {
+    element2.classList.remove("out");
+  }
+  count2++;
+  // if you're affraid the code could run for so long that it could fail,
+  // 6 is enough (number of elems * 2)
+  if (count2 === symbols.length * 2) {
+      count2 = 0;
+  }
+};
+let inthandle2 = setInterval(iteration2, 2500);
+iteration2();
+
+
+// second one 
+let count3 = 2;
+const element3 = document.getElementById("change3");
+const iteration3 = () => {
+  element3.innerHTML = symbols[parseInt(count2 / 2, 10) % symbols.length];
+  if (count3 % 2 !== 0) {
+    element3.classList.add("out");
+  } else {
+    element3.classList.remove("out");
+  }
+  count3++;
+  // if you're affraid the code could run for so long that it could fail,
+  // 6 is enough (number of elems * 2)
+  if (count3 === symbols.length * 2) {
+      count3 = 0;
+  }
+};
+let inthandle3 = setInterval(iteration3, 2500);
+iteration3();
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,26 +1,6 @@
 // // express router
 
-// make background change
 
-// const symbols = ['HTML', 'CSS', 'JavaScript','Ai'];
-// let count = 0;
-// const element = document.getElementById("change");
-// const iteration = () => {
-//   element.innerHTML = symbols[parseInt(count / 2, 10) % symbols.length];
-//   if (count % 2 !== 0) {
-//     element.classList.add("out");
-//   } else {
-//     element.classList.remove("out");
-//   }
-//   count++;
-//   // if you're affraid the code could run for so long that it could fail,
-//   // 6 is enough (number of elems * 2)
-//   if (count === symbols.length * 2) {
-//       count = 0;
-//   }
-// };
-// let inthandle = setInterval(iteration, 1000);
-// iteration();
 
 // responsive navigation
 
@@ -42,4 +22,3 @@ function popupFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
-
